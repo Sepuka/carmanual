fed68cf9b27b4cc112836eb7e4987a261a6a89b9
shlomin <shlomin@1mps.ru>
2013-11-06 16:34:42 +0400

    Включен режим отладки и отлажено простое приложение
    Приложение создает намерение открыть адрес http://шломин.рф

--- carmanualProject/src/app/carmanual/MainActivity.java
+++ carmanualProject/src/app/carmanual/MainActivity.java
@@ -2,10 +2,7 @@ package app.carmanual;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.CheckBox;
-import android.content.ContentValues;
-import android.content.ContentResolver;
+import android.content.Intent;
 import android.net.Uri;
 
 public class MainActivity extends Activity
@@ -15,15 +12,8 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
-        //TODO: Получить список файлов из папки и выдать их списком для выбора
-        TextView tv = new TextView(this);
-        //ContentValues values = new ContentValues();
-        //values.put("key", "my value");
-        //ContentResolver cr = this.getContentResolver();
-        //Uri newUri = cr.insert(Uri.parse("content://app.carmanual.testdb"), values);
-        String string = "some text кириллица";
-        tv.setText(string);
-        CheckBox checkbox = new android.widget.CheckBox(this);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("http://шломин.рф"));
+        this.startActivity(intent);
     }
 }
